fa9b7dc10751911ee777bed82b25b72fb205ff20
Lukas Haible <lukas.haible@web.de>
2025-06-22 16:41:46 +0200

    API (/) Route

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,1 +1,13 @@
 package router
+
+import (
+	"github.com/Pixel-DB/Pixel-DB-API/internal/handler"
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupRouter(app *fiber.App) {
+
+	api := app.Group("/") //Main Route
+	api.Get("/", handler.Hello)
+
+}
