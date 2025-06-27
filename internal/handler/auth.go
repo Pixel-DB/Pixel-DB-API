48d8be9eb83d9bd9a496afa81bc1b7c931211b33
Lukas Haible <lukas.haible@web.de>
2025-06-27 12:09:38 +0200

    Login system, check hashed password, response

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -1,7 +1,39 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/Pixel-DB/Pixel-DB-API/internal/database"
+	"github.com/Pixel-DB/Pixel-DB-API/internal/dto"
+	"github.com/Pixel-DB/Pixel-DB-API/internal/model"
+	"github.com/Pixel-DB/Pixel-DB-API/internal/security"
+	"github.com/gofiber/fiber/v2"
+)
 
 func Login(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{"status": "success", "message": "Login"})
+	type LoginInput struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	input := new(LoginInput)
+
+	if err := c.BodyParser(input); err != nil {
+		return c.JSON(fiber.Map{"status": "Error"})
+	}
+
+	userModel := new(model.Users)
+
+	db := database.DB
+	db.Where(model.Users{Email: input.Email}).First(userModel)
+
+	if !security.CheckPasswordHash(userModel.Password, input.Password) {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"status": "error", "message": "Invalid identity or password", "data": nil})
+	}
+
+	AuthResponse := dto.AuthResponse{
+		ID:       userModel.ID,
+		Email:    userModel.Email,
+		Username: userModel.Username,
+		Role:     userModel.Role,
+	}
+
+	return c.JSON(fiber.Map{"status": "success", "message": "Logged in", "data": AuthResponse})
 }
