1640b4686d964e26ae643f9e267d33295b97fd89
Lukas Haible <lukas.haible@web.de>
2025-07-03 20:14:41 +0200

    Switching PASSWORD and USERNAME

--- internal/utils/initMinioClient.go
+++ internal/utils/initMinioClient.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"fmt"
+
 	"github.com/Pixel-DB/Pixel-DB-API/config"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
@@ -8,8 +10,9 @@ import (
 
 func InitMinioClient() (*minio.Client, error) {
 	minioClient, err := minio.New("minio-dev:9000", &minio.Options{
-		Creds:  credentials.NewStaticV4(config.Config("MINIO_PASSWORD"), config.Config("MINIO_USER"), ""),
+		Creds:  credentials.NewStaticV4(config.Config("MINIO_USER"), config.Config("MINIO_PASSWORD"), ""),
 		Secure: false,
 	})
+	fmt.Println("Initializing MinIO client...")
 	return minioClient, err
 }
