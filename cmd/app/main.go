9ab5af9e01df4ab2aac64806511d79d2b6d99869
Lukas Haible <lukas.haible@web.de>
2025-07-31 04:31:03 +0200

    Swagger Setup

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-	"log"
-
 	"github.com/Pixel-DB/Pixel-DB-API/config"
+	_ "github.com/Pixel-DB/Pixel-DB-API/docs"
 	"github.com/Pixel-DB/Pixel-DB-API/internal/database"
 	"github.com/Pixel-DB/Pixel-DB-API/internal/router"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"log"
 )
 
+// @title Pixel Schwanz
+// @version 1.1
+// @description Pixel
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email fiber@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:3000
+// @BasePath /
 func main() {
 	app := fiber.New()
 
