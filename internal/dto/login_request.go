cfd1243286afb9799a1126aa6e427143b6a43cff
Lukas Haible <lukas.haible@web.de>
2025-08-02 22:25:23 +0200

    Struct Validation

--- internal/dto/login_request.go
+++ internal/dto/login_request.go
@@ -1,6 +1,6 @@
 package dto
 
 type LoginRequest struct {
-	Email    string `json:"email" validate:"email,required"`
-	Password string `json:"password"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6,max=70"`
 }
