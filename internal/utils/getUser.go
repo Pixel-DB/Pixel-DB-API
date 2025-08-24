7802cbf677d87b658a3219e75162e37f03d0883f
Lukas Haible <lukas.haible@web.de>
2025-08-24 17:11:24 +0200

    Get User

--- internal/utils/getUser.go
+++ internal/utils/getUser.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/Pixel-DB/Pixel-DB-API/internal/database"
 	"github.com/Pixel-DB/Pixel-DB-API/internal/model"
@@ -11,9 +12,10 @@ import (
 func GetUser(i string) (*model.Users, error) { //Get user by ID
 	u := new(model.Users)
 	db := database.DB
-	if err := db.Where(&model.Users{ID: i}).First(u).Error; err != nil {
+	err := db.First(u, "id = ?", i).Error
+	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return nil, nil
+			return nil, fmt.Errorf("User with ID %s not found", i)
 		}
 		return nil, err
 	}
