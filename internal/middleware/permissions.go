4352146f73c8352dac01cc1a3848ea87efbfe668
Lukas Haible <lukas.haible@web.de>
2025-09-09 23:47:52 +0200

    Check Role Perms

---
+++ internal/middleware/permissions.go
@@ -0,0 +1,26 @@
+package middleware
+
+var rolePermissions = map[string][]string{
+	"user": {
+		"pixelart.upload",
+	},
+	"moderator": {
+		"pixelart.upload",
+		"pixelart.review",
+	},
+	"admin": {
+		"pixelart.upload",
+		"pixelart.review",
+		"user.delete",
+	},
+}
+
+func HasPermission(role string, permission string) bool {
+	permissions := rolePermissions[role]
+	for _, p := range permissions {
+		if p == permission {
+			return true
+		}
+	}
+	return false
+}
